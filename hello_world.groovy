079b0c5e8475fc0372a5d2edcf3ab22b67ec15a7
Piotr Szubiakowski <pszubiak@eso.org>
2019-06-11 15:26:16 +0200

    Adds response.

--- hello_world.groovy
+++ hello_world.groovy
@@ -1,11 +1,18 @@
 // Creates or updates a free style job.
 job("hello-world") {
 
+    // Allows to parameterize the job.
+    parameters {
+
+        // Defines a simple text parameter, where users can enter a string value.
+        stringParam('response', 'Hello Jenkins!', 'Worlds response')
+    }
+
     // Adds build triggers to the job.
     triggers {
 
         // Triggers the job based on regular intervals.
-        cron("@daily")
+        cron('@daily')
     }
 
     // Adds build steps to the jobs.
@@ -14,4 +21,13 @@ job("hello-world") {
         // Runs a shell script.
         shell('echo Hello World!')
     }
+
+    // Adds build steps to the jobs.
+    steps {
+
+        // Runs a shell script.
+        shell('echo Hello $response')
+    }
+
+
 }
