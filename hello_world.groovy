697ac7bcb72a41c08e90f2719b5c65913d6e8fb7
Piotr Szubiakowski <pszubiak@eso.org>
2019-06-11 15:46:04 +0200

    Removes response.

--- hello_world.groovy
+++ hello_world.groovy
@@ -1,13 +1,6 @@
 // Creates or updates a free style job.
 job("hello-world") {
 
-    // Allows to parameterize the job.
-    parameters {
-
-        // Defines a simple text parameter, where users can enter a string value.
-        stringParam('response', 'Hello Jenkins!', 'Worlds response')
-    }
-
     // Adds build triggers to the job.
     triggers {
 
@@ -21,13 +14,5 @@ job("hello-world") {
         // Runs a shell script.
         shell('echo Hello World!')
     }
-
-    // Adds build steps to the jobs.
-    steps {
-
-        // Runs a shell script.
-        shell('echo Hello $response')
-    }
-
 
 }
