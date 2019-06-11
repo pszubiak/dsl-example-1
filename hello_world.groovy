a61ab909c12cacdb54e2c6ad2c4e71346e3acb05
Piotr Szubiakowski <pszubiak@eso.org>
2019-06-11 14:13:33 +0200

    Adds hello_world.groovy example.

---
+++ hello_world.groovy
@@ -0,0 +1,17 @@
+// Creates or updates a free style job.
+job("Hello-World") {
+
+    // Adds build triggers to the job.
+    triggers {
+
+        // Triggers the job based on regular intervals.
+        cron("@daily")
+    }
+
+    // Adds build steps to the jobs.
+    steps {
+
+        // Runs a shell script.
+        shell('echo Hello World!')
+    }
+}
